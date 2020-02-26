6348dc7d9f24a36b29875e90c5aff35e9d44b41b
vaal <vaal.87@gmail.com>
2020-02-26 23:21:16 +0300

    координаты границы зоны теперь не содержат высоту

--- src/mission_report/helpers.py
+++ src/mission_report/helpers.py
@@ -6,9 +6,9 @@ def point_in_polygon(point, polygon):
     x, y = point['x'], point['z']
     n = len(polygon)
     inside = False
-    p1x, _, p1y = polygon[0]
+    p1x, p1y = polygon[0]
     for i in range(n + 1):
-        p2x, _, p2y = polygon[i % n]
+        p2x, p2y = polygon[i % n]
         if min(p1y, p2y) < y <= max(p1y, p2y) and x <= max(p1x, p2x):
             if p1y != p2y:
                 xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
